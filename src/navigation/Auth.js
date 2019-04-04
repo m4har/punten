e2cac2f7b6e90103c7a4a5a4813095e05f2932a4
Mahardicka <mahardicka404@gmail.com>
2019-04-04 22:44:18 +0700

    test magic item

--- src/navigation/Auth.js
+++ src/navigation/Auth.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { connect } from "react-redux";
+import { View } from "react-native";
 
 class Auth extends React.Component {
-  render() {
+  componentDidMount() {
+    console.log(this.props);
     if (this.props.Register.data === "") {
       return this.props.navigation.navigate("Login");
     } else {
       return this.props.navigation.navigate("Home");
     }
+  }
+  render() {
+    return <View />;
   }
 }
 const MapStateToProps = state => ({
